dcf2ec0211f69aa7c83d9bcc14c2ae288261f483
jaden <cep450@nyu.edu>
2021-05-10 00:55:29 -0400

    talk indicator (placeholder) appears if player approaches NPC

--- Assets/Scripts/DialogueStart.cs
+++ Assets/Scripts/DialogueStart.cs
@@ -7,8 +7,10 @@ public class DialogueStart : MonoBehaviour
 
     //colliders and triggers and whatever need a rigidbody which i don't want to deal w physics...
     //so i'm just doing the math manually like "is the distance between the player and the NPC whatever"
-    float playerNearbyRadius = 2f;
+    float playerNearbyRadius = 2.5f;
     public Transform playerTransform;
+
+    bool indicatorDisplayed = false;
 
 
     /* Start is called before the first frame update
@@ -21,17 +23,47 @@ public class DialogueStart : MonoBehaviour
     void Update()
     {
 
-        //when the player presses [interact key] (E, space, click, enter.... all of these)
-        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.E)) {
+        //are they in our circle?
+        if(Vector3.Distance(playerTransform.position, transform.position) <= playerNearbyRadius){
 
-            //are they in our circle?
-            if(Vector3.Distance(playerTransform.position, transform.position) <= playerNearbyRadius){
+            //show that this person can be talked to! 
+            //if not displayed, display
+            if(!indicatorDisplayed) {
+                //display the indicator
+                Renderer [] renderers = GetComponentsInChildren<Renderer>();
+                foreach(Renderer r in renderers) {
+                    if(r.tag.Equals("TalkIndicator")) {
+                        r.enabled = true;
+                    }
+                }
+                indicatorDisplayed = true;
+            }
+
+            //when the player presses [interact key] (E, space, click, enter.... all of these)
+            if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.E)) {
+
+                //stop showing the indicator
+                //TODO
 
                 //open up the dialogue window and start!
                 Debug.Log("hello!");
                 //TODO 
 
             }
+        } else {
+            
+            //we're not in their circle 
+            //if the indicator is displayed, stop displaying it.
+            if(indicatorDisplayed) {
+                Renderer [] renderers = GetComponentsInChildren<Renderer>();
+                foreach(Renderer r in renderers) {
+                    if(r.tag.Equals("TalkIndicator")) {
+                        r.enabled = false;
+                    }
+                }
+                indicatorDisplayed = false;
+            }
+
         }
     }
 }
