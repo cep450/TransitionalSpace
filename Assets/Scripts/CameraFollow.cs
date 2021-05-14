279aff8cd37269663002ed90e21b2f77b531f5d2
jaden <cep450@nyu.edu>
2021-05-13 21:24:58 -0400

    camera follow, deadzones

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    //camera will only move side to side, and will stop at certain values.
+
+
+    //stop moving here
+    float negMax = -10f;
+    float posMax = 10f;
+
+    //deadzone size
+    float deadzone = 2.5f;
+    
+    float lastPlayerPosition;
+    float playerPositionDiff = 0f;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPlayerPosition = player.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        playerPositionDiff = player.position.x - lastPlayerPosition;
+        lastPlayerPosition = player.position.x;
+
+        //has the player gone outside of our deadzone?
+        if((player.position.x > transform.position.x + deadzone && transform.position.x + playerPositionDiff < posMax
+        && playerPositionDiff > 0f) || 
+        (player.position.x < transform.position.x - deadzone && transform.position.x + playerPositionDiff > negMax
+        && playerPositionDiff < 0f)) {
+            transform.Translate(Vector3.right * playerPositionDiff);
+
+        }
+        
+    }
+}
