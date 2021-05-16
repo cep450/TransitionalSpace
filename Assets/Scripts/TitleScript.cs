dbb354702c76d8b914cf03f28d75e9c038d1deb5
jaden <cep450@nyu.edu>
2021-05-15 23:51:36 -0400

    script files set up and linked, intro bit set up to implement tomorrow

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour
+{
+
+    //when a key is pressed, move the camera down
+    //then, fade away the grey thing 
+
+    public Transform cameraTransform;
+    public Sprite greySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
