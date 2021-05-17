b5c2eaf8401239f7f87c46c80782573e998b4cf5
jaden <cep450@nyu.edu>
2021-05-17 16:12:05 -0400

    talk indicators and controls indicators working and sprites added

--- Assets/Scripts/DialogueStart.cs
+++ Assets/Scripts/DialogueStart.cs
@@ -15,13 +15,6 @@ public class DialogueStart : MonoBehaviour
 
 
     public MyNPC target;
-    
-
-    /* Start is called before the first frame update
-    void Start()
-    {
-        
-    }*/
 
     // Update is called once per frame
     void Update()
